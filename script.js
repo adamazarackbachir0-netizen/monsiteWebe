18018d3acbb50867dd8bd43034b4cfb60501eaaa
Adam Mahamat Azarack <adamazarckbachir0@gmail.com>
2026-01-25 15:02:08 +0100

    second line of commit message.

--- script.js
+++ script.js
@@ -74,3 +74,22 @@ function goToCheckout() {
 }
 
 displayCart();
+// FIN PANIER
+// Chargement des produits depuis productslist.json
+fetch('productslist.json')
+    .then(response => response.json())
+    .then(products => {
+        const productsContainer = document.getElementById('products-container');
+        products.forEach(product => {
+            const productCard = document.createElement('div');
+            productCard.className = 'product-card';
+            productCard.innerHTML = `
+                <img src="${product.url}" alt="${product.name}">
+                <h3>${product.name}</h3>
+                <p>Price: ${product.price}</p>
+                <button onclick="addToCart('${product.name}', ${parseInt(product.price)})">Add to Cart</button>;
+            `;
+            productsContainer.appendChild(productCard);
+        }); 
+        })
+    .catch(error => console.error('Error loading products:', error));
